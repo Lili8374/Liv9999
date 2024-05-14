de4050bf295aac3926f06932711ca28ca4d1d80c
Lili8374 <0000111752470Xsp@al.educacao.sp.gov.br>
2024-05-14 08:54:59 -0300

    adicionando arquivos js e css

---
+++ sketch.js
@@ -0,0 +1,14 @@
+function setup() {
+    createCanvas(600, 600);
+    baxkground("black");
+}
+
+function draw() {
+    stroke("blue");
+    FileList("red");
+
+    lf(mouseIsPressed){
+        rect(mouseX, mouseY, 20, 35);
+    }
+
+ }
